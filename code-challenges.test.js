0b72d8c57eefad2607f28e946f564ddaa83ac776
aclassiter <a.class.wa@gmail.com>
2022-05-10 06:57:43 -0700

    Completed week 6 assessment.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,38 +14,107 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
-const people = [
-  { name: "ford prefect", occupation: "a hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+describe("capitalizedNames", () => {
+  //describe what the test does
+  it("Takes in an array of objects and returns an array with a sentence about each person with their name capitalized", () => {
+    //variable given
+    const people = [
+      { name: "ford prefect", occupation: "a hitchhiker" },
+      { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+      { name: "arthur dent", occupation: "a radio employee" }
+    ]
+  //expected outputs
+  expect(capitalizedNames(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+})
+})
 
+// //Fail received:
+//    ✕ Takes in an array of objects and returns an array with a sentence about each person with their name capitalized (1 ms)
+//ReferenceError: capitalizedNames is not defined
 
 // b) Create the function that makes the test pass.
+//Function name: capitalizedNames
+//Input: array of objects
+//Output: sentence including person's name and occupation, with their name capitalized
+//
+const capitalizedNames = (array) => {
+  return `${people.name} is ${people.occupation}`
+}
 
+console.log(capitalizedNames(people))
+
+//I'm receiving a "people is not defined" error. I am referencing the JavaScript Objects material in our syllabus, which has an example a little similar to this one. Could it be that I have to call upon each name individually? 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-// Expected output: [ 2, 0, -1, 0 ]
-const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-// Expected output: [ 2, 1, -1 ]
+describe("remaindersOnly", () => {
+  //describe what the test does
+  it("Takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3."
+  , () => {
+    //variable given
+     const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+    //Expected output: [ 2, 0, -1, 0 ]
+     const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+    //Expected output: [ 2, 1, -1 ]
+
+  //expected outputs
+  expect(remaindersOnly(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+  expect(remaindersOnly(hodgepodge2)).toEqual([ 2, 1, -1 ])
+})
+})
+// //Fail received:
+// //✕ Takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3. (1 ms)
+// // ReferenceError: remaindersOnly is not defined
 
 
-// b) Create the function that makes the test pass.
+// // b) Create the function that makes the test pass.
+// //Function name: remaindersOnly
+// //Input: mixed data array
+// //Output: remainders of the numbers when divided by 3
+// //Use .filter method to filter the numbers from the array
+// //Use modulo to find remainder of the numbers when divided by 3
+const remaindersOnly = arr.filter; (Number) => {
+   return Number % 3
 
+}
+console.log(remaindersOnly(hodgepodge1))
+console.log(remaindersOnly(hodgepodge2))
+//I think I'm close to getting the correct function to make the test pass. I am receiving an error that states "arr is not defined". 
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-const cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
+describe("sumsCubed", () => {
+  //describe what the test does
+  it("Takes in an array of numbers and returns the sum of all the numbers cubed."
+  , () => {
+    //variable given
+     const cubeAndSum1 = [2, 3, 4]
+    // Expected output: 99
+     const cubeAndSum2 = [0, 5, 10]
+    // Expected output: 1125
 
+  //expected outputs
+  expect(sumsCubed(cubeAndSum1)).toEqual(99)
+  expect(sumsCubed(cubeAndSum2)).toEqual(1125)
+})
+})
+//Fail received:
+// ✕ Takes in an array of numbers and returns the sum of all the numbers cubed.
+// ReferenceError: sumsCubed is not defined
 
 // b) Create the function that makes the test pass.
+//Function name: sumsCubed
+//Input: array of numbers
+//Output: sum of all the numbers cubed
+ function sumsCubed(numbers) {
+   return numbers.map(x => x ** 3)
+ }
+ console.log(sumsCubed(cubeAndSum1))
+
+//I'm first trying to make a function that cubes all the numbers, then I will figure out how to add the numbers and return the sum. The parameter is numbers, the .map method is there to return an array with the same amount of numbers, and the x ** 3 is indicative of a number to the third (cubed) power. Every time I run this I receive an error that states "cubeAndSum1 is not defined".
+
+//This week's assessment showed me that I really need to take the time to review JavaScript. I was unfortunately unable to create working functions in this section, but I feel like I got close to the right answers. 
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -8,38 +8,49 @@
 
 # ---1)
 class BlogPostsController < ApplicationController
-  def index
+  def index #the R in CRUD
+# BlogPostsController is the main controller, which inherits from ApplicationController
+# index is a method that makes an Active Record call 
     # ---2)
     @posts = BlogPost.all
-  end
+  end #every def has an end
+# saving all the content (.all) from the database as an instance variable
 
   # ---3)
   def show
     @post = BlogPost.find(params[:id])
-  end
+  end #every def has an end
+  #Show is a RESTful route that looks for one item in the database. Inside the show method we will make an Active Record call that finds one item by id ([:id]). The id comes from the url params. Here, we are looking for a single post. 
 
   # ---4)
-  def new
+  def new #midway between C and R in CRUD
     @post = BlogPost.new
-  end
+  end #every def has an end
+  #new is a RESTful route that displays a form for the user, which, in this case, is a blog post
 
-  def create
+  def create #the C in CRUD
+  #create is a RESTful route that submits user data to the database (the data filled out in the post)
     # ---5)
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
+    # _path indicates that we are referencing the route alias "blog_post"
     else
       redirect_to new_blog_post_path
+    # new_blog_post is another alias
     end
-  end
+  end#every def has an end
 
   def edit
+  # edit is a RESTful route that allows users to edit an item as determined by its id. 
     # ---6)
     @post = BlogPost.find(params[:id])
-  end
+    #A blog post is located for editing (adding information to the database)
+  end #every def has an end
 
-  def update
+  def update #the U in CRUD
     @post = BlogPost.find(params[:id])
+    #A blog post is located for updating
     # ---7)
     @post.update(blog_post_params)
     if @post.valid?
@@ -47,22 +58,27 @@ class BlogPostsController < ApplicationController
     else
       redirect_to edit_blog_post_path
     end
-  end
+  end #every def has an end
+  #The blog post is updated
 
-  def destroy
+  def destroy #the D in CRUD
     @post = BlogPost.find(params[:id])
+    # A blog post is located by its id for deletion
     if @post.destroy
       redirect_to blog_posts_path
     else
       # ---8)
       redirect_to blog_post_path(@post)
+    # information is removed from the database (a blog post is deleted/destroyed)
     end
-  end
+  end #every def has an end
 
   # ---9)
   private
   def blog_post_params
+  #Private means that all methods defined from now on are to be private. They can only be called from within the object (from other methods that the class defines). 
     # ---10)
     params.require(:blog_post).permit(:title, :content)
-  end
+    # Blog posts are required to have a title and content (the params)
+  end #every def has an end
 end
